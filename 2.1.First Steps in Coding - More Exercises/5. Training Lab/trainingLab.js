bd47d806579e2df902fd3b3d655e089bb018a2e1
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-25 15:52:18 +0200

    Create trainingLab.js

---
+++ 2.1.First Steps in Coding - More Exercises/5. Training Lab/trainingLab.js
@@ -0,0 +1,9 @@
+function trainingLab (input){
+    const w = Number(input[0])*100;
+    const h = Number(input[1])*100;
+
+    const tableInRow = (h - 100)/70;
+    const tableInColoms = w / 120;
+    const allTables = (((Math.floor(tableInColoms))* ( Math.floor(tableInRow))) - 3);
+    console.log(allTables);
+}
