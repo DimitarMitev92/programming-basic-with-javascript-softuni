6a863cb100904141dd5b68c04b3e301f5de46d30
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-25 15:52:58 +0200

    Create fishland.js

---
+++ 2.1.First Steps in Coding - More Exercises/6. Fishland/fishland.js
@@ -0,0 +1,16 @@
+function fishLand(input){
+    const priceMackerelPerKg = Number(input[0]); 
+    const priceSpratPerKg = Number(input[1]);   
+    const kgBonito = Number(input[2]);         
+    const kgScad = Number(input[3]);          
+    const kgMussels = Number(input[4]);      
+
+
+    let priceBonito = priceMackerelPerKg*1.6;
+    let sumBonito = priceBonito * kgBonito;
+    let priceScad = priceSpratPerKg*1.8;
+    let sumScad = priceScad * kgScad;
+    let sumMussels = 7.5 * kgMussels;
+    let totalSum = sumBonito + sumScad + sumMussels;
+    console.log(totalSum.toFixed(2));
+}
