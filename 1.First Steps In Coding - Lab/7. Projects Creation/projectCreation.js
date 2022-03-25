6979cca841da4d679c731c4eecbb10d61b927506
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-25 15:41:31 +0200

    Create projectCreation.js

---
+++ 1.First Steps In Coding - Lab/7. Projects Creation/projectCreation.js
@@ -0,0 +1,5 @@
+function projectsCreations (input){
+    const name = input[0];
+    const projectsNum = Number(input[1]);
+    console.log(`The architect ${name} will need ${projectsNum*3} hours to complete ${projectsNum} project/s.`)
+}
