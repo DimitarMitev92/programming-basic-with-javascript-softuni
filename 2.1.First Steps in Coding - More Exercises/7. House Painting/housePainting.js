4a718e60a9229d28d3bc0c962df217d1ec4d4b22
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-26 17:00:06 +0200

    Update housePainting.js

--- 2.1.First Steps in Coding - More Exercises/7. House Painting/housePainting.js
+++ 2.1.First Steps in Coding - More Exercises/7. House Painting/housePainting.js
@@ -3,13 +3,13 @@ function housePainting(input){
     const y = Number(input[1]);
     const h = Number(input[2]);
 
-    const prednaIZadnaStrana = (2*x*x) - (1.2*2);
-    const stranichni = (2*x*y) - (2*1.5*1.5);
-    const zeleno = prednaIZadnaStrana + stranichni;
-    console.log((zeleno / 3.4).toFixed(2));
+    const frontAndBackSides = (2*x*x) - (1.2*2);
+    const sides = (2*x*y) - (2*1.5*1.5);
+    const green = frontAndBackSides + sides;
+    console.log((green / 3.4).toFixed(2));
 
-    const triygylnici = (x*h/2)*2;
-    const pokrivPravoygylnik = 2*x * y;
-    const cherveno = triygylnici + pokrivPravoygylnik;
-    console.log((cherveno/4.3).toFixed(2)); 
+    const triangles = (x*h/2)*2;
+    const roofRectangle = 2*x * y;
+    const red = triangles + roofRectangle;
+    console.log((red/4.3).toFixed(2)); 
 }
