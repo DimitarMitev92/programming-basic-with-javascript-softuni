64a197c0f919d798861307bf4e7d5907d652ef54
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-26 16:48:20 +0200

    Update repainting.js

--- 2.First Steps In Coding - Exercise/6. Repainting/repainting.js
+++ 2.First Steps In Coding - Exercise/6. Repainting/repainting.js
@@ -1,14 +1,14 @@
-function repainting (input){
-    let nailon = Number(input[0]);
-    let boq = Number(input[1]);
-    let razreditel = Number(input[2]);
-    let chasove = Number(input[3]);
+function repainting(input){
+    let nylon = Number(input[0]);
+    let paint = Number(input[1]);
+    let thinner = Number(input[2]);
+    let hours= Number(input[3]);
 
-    let totalNailon = (nailon + 2) * 1.5;
-    let totalBoq = (boq + 0.1*boq)*14.5;
-    let totalRazreditel = razreditel * 5;
-    let torbichka = 0.4;
-    let finalSum = totalNailon + totalBoq + totalRazreditel + torbichka;
-    let sumMaistori = ( finalSum*0.3)*chasove;
-    console.log(finalSum + sumMaistori);
+    let totalNylon = (nylon + 2) * 1.5;
+    let totalPaint = (paint + 0.1*paint)*14.5;
+    let totalThinner = thinner * 5;
+    let bag = 0.4;
+    let finalSum = totalNylon + totalPaint + totalThinner + bag;
+    let sumWorkers = ( finalSum*0.3)*hours;
+    console.log(finalSum + sumWorkers);
 }
