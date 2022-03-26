ec1f1fd921bd146bda2572d193671c7f2775590e
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-26 16:40:00 +0200

    Update inchesToCentimeters.js

--- 1.First Steps In Coding - Lab/4. Inches to Centimeters/inchesToCentimeters.js
+++ 1.First Steps In Coding - Lab/4. Inches to Centimeters/inchesToCentimeters.js
@@ -1,4 +1,4 @@
-function inchesToCentimeters (input){
+function inchesToCentimeters(input){
     const inches = Number(input[0]);
     const cantimeters = inches * 2.54;
     console.log(cantimeters);
