58d0c3e5ff5f193f7e55ec8c4064a1544d65ee7d
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-26 16:41:26 +0200

    Update petShop.js

--- 1.First Steps In Coding - Lab/8. Pet Shop/petShop.js
+++ 1.First Steps In Coding - Lab/8. Pet Shop/petShop.js
@@ -1,4 +1,4 @@
-function zoomarket (input){
+function petShop(input){
     const dogFood = Number(input[0]);
     const catFood = Number(input[1]);
     console.log(`${((dogFood*2.5) + (catFood* 4))} lv.`)
