7689155b307373aa02eed0c3ae0c25c7ae0aa0b9
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-26 16:53:37 +0200

    Update depositCalculatior.js

--- 2.First Steps In Coding - Exercise/3. Deposit Calculator/depositCalculatior.js
+++ 2.First Steps In Coding - Exercise/3. Deposit Calculator/depositCalculatior.js
@@ -3,8 +3,8 @@ function depositCalculator(input){
     const valueOfDeposit = Number(input[1]);
     const yearPercent = Number(input[2]) / 100;
 
-    let lihva = depositSum * yearPercent;
-    let lihvaZa1Mesec = lihva / 12;
-    let TotalSum = depositSum + valueOfDeposit*lihvaZa1Mesec;
-    console.log(TotalSum.toFixed(2));
+    let interest = depositSum * yearPercent;
+    let interestPerMonth = interest / 12;
+    let totalSum = depositSum + valueOfDeposit*interestPerMonth;
+    console.log(totalSum.toFixed(2));
 }
