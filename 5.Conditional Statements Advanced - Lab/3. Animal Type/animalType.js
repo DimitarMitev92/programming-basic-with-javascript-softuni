02f050bec820d6c8cdd5e0c4a9119b617216c82b
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-03-30 08:56:49 +0300

    Create animalType.js

---
+++ 5.Conditional Statements Advanced - Lab/3. Animal Type/animalType.js
@@ -0,0 +1,15 @@
+function typeAnimal (input){
+    let name = input[0];
+
+    switch ( name ){
+        case "dog": console.log("mammal"); break;
+        case "crocodile":
+        case "tortoise":
+        case "snake":
+        console.log("reptile");
+        break;
+        default:
+        console.log("unknown");
+        break;
+    }
+}
