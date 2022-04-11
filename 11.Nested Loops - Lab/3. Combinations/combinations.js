b3768267544361448eb2ad11fc7144eabee65f45
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-04-11 10:14:11 +0300

    Create combinations.js

---
+++ 11.Nested Loops - Lab/3. Combinations/combinations.js
@@ -0,0 +1,15 @@
+function combination(input){
+    let n = Number(input[0]);
+    let counter=0;
+    for ( let i = 0; i <= n; i++){
+        for( let j = 0; j <=n; j++){
+            for( let k = 0 ; k <= n; k++){
+                let sum = i + j + k;
+                if(sum === n){
+                    counter++;
+                }
+            }
+        }
+    }
+    console.log(counter)
+}
