cbdb0ee3adeececf69e271f0d3f393557d16fce2
Dimitar Mitev <100955484+DimitarMitev92@users.noreply.github.com>
2022-05-09 10:27:13 +0300

    Create competition.js

---
+++ 13.Exams/6.Programming Basics Online Exam - 20 and 21 April 2019/6. Easter Competition/competition.js
@@ -0,0 +1,42 @@
+function easter(input) {
+    let index = 0;
+    let easterBread = Number(input[index]);
+    index++
+    let command = input[index];
+    index++
+    let points = 0
+    let localPoints = 0;
+    let counter = 0;
+    let bestChef = '';
+    let bestScore = 0;
+
+    for (let i = 0; i < easterBread; i++) {
+        counter++
+        points = 0;
+        let chef = command
+        command = input[index];
+        index++;
+
+
+        while (command !== 'Stop') {
+            localPoints = Number(command);
+            points += localPoints
+
+            command = input[index];
+            index++
+        }
+        
+        console.log(`${chef} has ${points} points.`);
+
+        if (points > bestScore) {
+            bestScore = points;
+            bestChef = chef
+            console.log(`${chef} is the new number 1!`);
+        }
+        
+        command = input[index];
+        index++;
+    }
+
+    console.log(`${bestChef} won competition with ${bestScore} points!`);
+}
